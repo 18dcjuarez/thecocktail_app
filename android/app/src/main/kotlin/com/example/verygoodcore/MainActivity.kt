747a9cc6fd095697c41f813bdf6dc29ebf6e71be
David Cruz Juarez <davidcruzjuarez@MacBook-Pro-de-David-2.local>
2022-12-27 10:01:07 -0600

    First commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.thecocktaildb_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:thecocktaildb_app/app/app.dart';
+import 'package:thecocktaildb_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
